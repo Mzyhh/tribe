32b8bb614b0b5eddf2e4817f8950360fac7921bc
Grigory Sedov <xiiad@mail.ru>
2025-04-28 00:26:41 +0300

    Fix tables

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,26 +1,28 @@
 CREATE TABLE IF NOT EXISTS family (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS people (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     birthday DATE,
     deathday DATE,
     description TEXT,
-    family_id INT,
+    family_id INTEGER,
     FOREIGN KEY (family_id) REFERENCES family (id)
 );
 
-CREATE TABLE IF NOT EXISTSD family_tie_types (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS family_tie_types (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS family_ties (
-    human_id1 INT,
-    human_id2 INT,
-    type INT,
-    FOREIGN KEY (type) REFERENCES family_tie_types (id)
+    human_id1 INTEGER,
+    human_id2 INTEGER,
+    type INTEGER,
+    FOREIGN KEY (type) REFERENCES family_tie_types (id),
+    FOREIGN KEY (human_id1) REFERENCES people (id),
+    FOREIGN KEY (human_id2) REFERENCES people (id)
 );
--- data/db_sessions.h
+++ data/db_sessions.h
@@ -2,13 +2,17 @@
 #define __DB_SESSIONS_H__
 #include <sqlite3.h>
 #include <string>
+#include <fstream>
 
+constexpr int ERROR_MSG_LEN = 40;
 
 class Session {
 public:
-    Session(const std::string path=":memory:");
-
-    ~Session();
+    Session(const std::string& path=":memory:");
+ 
+    void execute(const std::ifstream& sql_file);
+    void execute(const std::string& sql_query);
+    ~Session() noexcept;
 private:
     sqlite3* DB;
 };
